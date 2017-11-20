0a4a8d4246813c235f7257a74c1866295b2f1833
ultrasaber <saberthesaber@gmail.com>
2017-11-20 02:20:22 +0000

    #16 - Add genre field to search form

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,9 @@
 class SearchController < ApplicationController
   def search
-    @songs = Song.where("title LIKE ? OR description LIKE ?", "%#{params[:q]}%", "%#{params[:q]}%")
+    if params[:genre] != ''
+      @songs = Genre.where(:id => params[:genre]).first.songs.where("title LIKE ? OR description LIKE ?", "%#{params[:q]}%", "%#{params[:q]}%")
+    else
+      @songs = Song.where("title LIKE ? OR description LIKE ?", "%#{params[:q]}%", "%#{params[:q]}%")
+    end
   end
 end
